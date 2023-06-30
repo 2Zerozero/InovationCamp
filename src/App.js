248d20029237ed36f05f0b1c4e289051c15de914
2Zerozero <hawgae@gmail.com>
2023-06-30 10:19:50 +0900

    로직 구현, 화면 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // state
+  const [todos, setTodos] = useState([{
+    id: 1,
+    content: "리액트는 어렵다",
+  }]);
+
+  const [inputTodo, setInputTodo] = useState('');
+
+  // handle
+  const handleInputChange = (e) => {
+    setInputTodo(e.target.value)
+  }
+  const handleInputSubmit = (e) => {
+    const newTodo = {
+      id: todos.length + 1, 
+      content: inputTodo,
+    };
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+    setInputTodo('');
+  }
+
+  console.log(todos)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+
+      {/* input */}
+      <div className="input">
+        <input type="text" name="content" value={inputTodo} onChange={handleInputChange}/>
+        <button onClick={handleInputSubmit}>추가하기</button>
+      </div>
+
+      {/* container */}
+      <div className="todolist">
+        <h1>Todo List</h1>
+        <div className="wrap">
+          {todos.map((todo) => {
+            return (
+              <div className='todo' key={todo.id}>
+                <h2>{todo.content}</h2>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
     </div>
   );
 }
