ae5dad5096a68e920c2710211579d0e41727c641
2Zerozero <hawgae@gmail.com>
2023-07-05 23:18:12 +0900

    코드 개선

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import { useState } from 'react';
-
-import * as S from './components/Layout/style.js'
-import Header from './components/Header/Header';
-import Form from './components/Form/Form';
-import Container from './components/Container/Container';
+import Router from "./shared/Router"
 
 function App() {
-  // State 관리
-  const [todos, setTodos] = useState([]);
-
-  console.log(todos)
   return (
-    <S.Layout>
-
-      {/* header */}
-      <Header />
-
-      {/* input */}
-      <Form 
-        todos={todos}
-        setTodos={setTodos}
-      />
-
-      {/* container */}
-      <Container
-        todos={todos}
-        setTodos={setTodos}
-      />
-
-    </S.Layout>
+    <Router />
   );
 }
 
