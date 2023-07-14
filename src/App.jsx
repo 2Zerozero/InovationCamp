b7989d460cc588e56867c0f7a681266d7533d533
Baek-Taejun <126327414+Baek-Taejun@users.noreply.github.com>
2023-07-15 02:45:46 +0900

    feat : 나브바,탭,메인 프레임 구현
    나브바,탭,메인 프레임 구현

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Nav from "./components/Nav";
+import Taps from './components/Taps';
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      <Taps/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav_styled.js
@@ -0,0 +1,25 @@
+import { styled, css } from "styled-components";
+
+const NavBox = styled.div`
+  display: flex;
+  padding-left: 20px;
+  padding-right: 20px;
+  justify-content: space-between;
+  font-size: 30px;
+  > div :not(:last-child) {
+    margin-right: 20px;
+  }
+`;
+
+const StBtn = styled.button`
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  font-size: 20px;
+  font-weight: 600;
+  cursor: pointer;
+  box-sizing: border-box;
+  border: none;
+`;
+
+export { NavBox, StBtn };
