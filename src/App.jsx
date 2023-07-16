11a1176a39a6293649909edf6f9b5da420a4c10d
2Zerozero <hawgae@gmail.com>
2023-07-17 01:42:42 +0900

    test : 디테일 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Detail from './pages/Detail/Detail';
 import Router from './shared/Router'
 
 function App() {
-  return <Router />;
+    return (
+        <Router />
+        // <Detail />
+    );
 }
 
 export default App;
