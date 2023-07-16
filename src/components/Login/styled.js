65a8d1e81feb7479776da30b6b3b7d26ab78b44f
2Zerozero <hawgae@gmail.com>
2023-07-16 18:07:39 +0900

    refactor : 폴더 수정

---
+++ src/components/Login/styled.js
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const Wrap = styled.div`
+    display: flex;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgb(255,255, 255, 0.9);
+`
