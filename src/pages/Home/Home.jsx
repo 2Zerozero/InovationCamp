d3720f18a569294f406436203a86aca6e4824ca9
2Zerozero <hawgae@gmail.com>
2023-07-16 19:54:10 +0900

    feat : Home 생성

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from '../../components/Nav'
+import Taps from '../../components/Taps'
+import Login from '../../components/Login/Login'
+
+function Home() {
+    return (
+        <>
+            <Nav />
+            <Taps />
+            <Login />
+        </>
+    )
+}
+
+export default Home
