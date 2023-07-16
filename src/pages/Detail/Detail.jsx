f6f45ba82dc313f25fcb6ea986e8303dd8f7ea57
2Zerozero <hawgae@gmail.com>
2023-07-16 19:54:03 +0900

    feat : Detail 생성

---
+++ src/pages/Detail/Detail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as S from './style'
+import Nav from '../../components/Nav'
+
+function Detail() {
+  return (
+    <S.Wrap>
+        <Nav />
+    </S.Wrap>
+  )
+}
+
+export default Detail
