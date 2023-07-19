3b5529bacc05756c3628717a2bc14b06deea5c8b
2Zerozero <hawgae@gmail.com>
2023-07-19 20:50:17 +0900

    fix : hover 누락

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -34,44 +34,45 @@ function Cards({ postId, id, title, isDone, content,createdDate,username,postIma
 
       <HoverCard key={id} style={{ width: '15rem' }}>
 
-      <Link to={`/api/posts/${postId}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-        <Card key={id} style={{ width: '15rem' }}>
-          
-          <Card.Img variant="top" src={postImageUrl} style={{ objectFit: 'cover', width: '100%', height: '170px' }}/>
-          
-          <Card.Body>
-            <Card.Title style={{ fontSize:"18px" , fontWeight: "600"}}>{title}</Card.Title>
+        <Link to={`/api/posts/${postId}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+          <Card key={id} style={{ width: '15rem' }}>
+            
+            <Card.Img variant="top" src={postImageUrl} style={{ objectFit: 'cover', width: '100%', height: '170px' }}/>
+            
             <Card.Body>
-              {content}
+              <Card.Title style={{ fontSize:"18px" , fontWeight: "600"}}>{title}</Card.Title>
+              <Card.Body>
+                {content}
+              </Card.Body>
             </Card.Body>
-          </Card.Body>
 
 
-          <ListGroup style={{ fontSize:'10px', marginBottom: '30px',display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-            <ListGroup.Item>{formattedCreatedDate}</ListGroup.Item>
-            <ListGroup.Item>0개의 댓글</ListGroup.Item>
-          </ListGroup>
+            <ListGroup style={{ fontSize:'10px', marginBottom: '30px',display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+              <ListGroup.Item>{formattedCreatedDate}</ListGroup.Item>
+              <ListGroup.Item>0개의 댓글</ListGroup.Item>
+            </ListGroup>
 
-          <S.CardBody style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-            <div style={{display: 'flex', alignItems: 'center'}}>
-          <Card.Img
-              variant="top"
-              src={postImageUrl}
-              style={{
-                objectFit: 'cover',
-                width: '1cm',
-                height: '1cm',
-                borderRadius: '50%',
-                border: '1px solid black',
-                marginRight: '10px', 
-              }}
-            />
-            <Card.Link >{username}</Card.Link>
-            </div>
-            <HeartLink ><AiFillHeart style={{ color: "red" }}/>{likeCount}</HeartLink>
-          </S.CardBody>
-        </Card >
-      </Link>
+            <S.CardBody style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+              <div style={{display: 'flex', alignItems: 'center'}}>
+            <Card.Img
+                variant="top"
+                src={postImageUrl}
+                style={{
+                  objectFit: 'cover',
+                  width: '1cm',
+                  height: '1cm',
+                  borderRadius: '50%',
+                  border: '1px solid black',
+                  marginRight: '10px', 
+                }}
+              />
+              <Card.Link >{username}</Card.Link>
+              </div>
+              <HeartLink ><AiFillHeart style={{ color: "red" }}/>{likeCount}</HeartLink>
+            </S.CardBody>
+          </Card >
+        </Link>
+      </HoverCard>
   </S.BootstrapCard>
   );
 }
